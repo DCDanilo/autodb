c2670b78df548a0733983cf71d4fa0145a64970c
DCDanilo <danilo.decave@gmail.com>
2022-11-26 14:04:22 +0100

    user profile init

---
+++ resources/views/user/profile.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <div>
+        <nav>
+            <div>
+                <img src="">
+            </div>
+        <!--  <ul>
+            <li></li>
+        </ul> -->
+        </nav>
+    </div>
+    <div>
+        <form action="">
+            <label for="name">Nome</label>
+        </form>
+    </div>
+</div>
