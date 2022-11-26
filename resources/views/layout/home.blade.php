61c4c7ecd074f7a7871e4b6e75ba90024afe5b66
DCDanilo <danilo.decave@gmail.com>
2022-11-26 14:03:40 +0100

    remove default home view and readme.md

---
+++ resources/views/layout/home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Home</title>
+</head>
+<body>
+    <div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Aggiungi Auto</li>
+            </ul>
+            <ul>
+                <li>Danilo</li>
+                <li>Login</li>
+                <li>Logout</li>
+            </ul>
+        </nav>
+    </div>
+
+    <div>
+        
+    </div>
+</body>
+</html>
